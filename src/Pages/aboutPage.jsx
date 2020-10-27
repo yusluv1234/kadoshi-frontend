c36d323d753943e300720fbadcaf47a24acd50a2
Anthony Oruamen <talktooruamen@gmail.com>
2020-10-27 13:34:58 +0100

    about page

--- src/Pages/aboutPage.jsx
+++ src/Pages/aboutPage.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import TopNav from '../components/topNav/topNav'
 import { FooterThin, FooterWide } from '../components/footer/footer'
-import ButtonSmall from '../components/buttonSmall/buttonSmall'
-import ButtonBig from '../components/buttonBig/buttonBig';
 
 const AboutPage = () => {
     return (
--- src/components/aboutBody/aboutBody.jsx
+++ src/components/aboutBody/aboutBody.jsx
@@ -1,1 +1,16 @@
-import React from 'react'
+import React from 'react';
+import './aboutBody.scss';
+
+const AboutBody = () => {
+    return (
+        <div className="about-body"> 
+            <p>"kADOSHI.com" is the brainchild of our imaginationthe we cherish greatly. it is something to generate a platform where employers/clients
+             and proffessionals can interact. It is another thing to create interaction in an enviromentthat potrays security, ease and transparency.
+             We work to bring a platform that creates adequate visibility and trust for professionals of choice and strategically, effortlessly and transparently. 
+             Our mission is to connect proffessionals to clients without restriction. Simply register and enjoy. 
+            </p>
+        </div>
+    );
+}
+
+export default AboutBody;
